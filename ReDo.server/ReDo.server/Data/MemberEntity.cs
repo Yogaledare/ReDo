cf443c906117be431fdd283986da266446f8f62e
Yogaledare <marcus_uppstrom@hotmail.com>
2024-05-11 01:01:13 +0200

    entities + EF

---
+++ ReDo.server/ReDo.server/Data/MemberEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace ReDo.server.Data;
+
+public class MemberEntity {
+    public int MemberId { get; set; }
+    public string FirstNAme { get; set; } = string.Empty; 
+    public int Age { get; set; }
+}
